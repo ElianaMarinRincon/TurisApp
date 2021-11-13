d0bfb69ac9d3b2e652d62921cb1b88956db6245d
Eliana Marín Rincón <90230434+ElianaMarinRincon@users.noreply.github.com>
2021-11-13 17:27:43 -0500

    Falta mejorar las ventanas y la prueba de usuario

--- app/src/main/java/com/example/turisapp/clases/UsuarioADO.java
+++ app/src/main/java/com/example/turisapp/clases/UsuarioADO.java
@@ -1,8 +1,15 @@
 package com.example.turisapp.clases;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.Nullable;
+
+import com.example.turisapp.modelos.usuario;
+
+import java.util.ArrayList;
 
 public class UsuarioADO extends SqliteConex{
 
@@ -14,6 +21,57 @@ public class UsuarioADO extends SqliteConex{
         this.contexto = c;
     }
 
-    public long insertar (Usuario us);
+    public long insertar (usuario us)
+    {
+        long id = 0;
+
+        try
+        {
+            SqliteConex dbc = new SqliteConex(this.contexto);
+            SQLiteDatabase db = dbc.getWritableDatabase();
+
+            ContentValues valores = new ContentValues();
+            valores.put("nombres", us.getNombres());
+            valores.put("apellidos", us.getApellidos());
+            valores.put("fechaNacimiento", us.getFechaNacimiento());
+            valores.put("correo", us.getCorreo());
+            valores.put("clave", us.getClave());
+
+            id = db.insert("usuarios", null, valores);
+        }
+
+        catch (Exception ex)
+        {
+
+        }
+
+        return id;
+    }
+
+    public ArrayList<usuario> listar()
+    {
+        SqliteConex conexion = new SqliteConex(this.contexto);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+
+        ArrayList<usuario> registros = new ArrayList<>();
+        Cursor cregistros = db.rawQuery("select id, nombres, apellidos, fechaNacimiento, coreo, clave from usuarios", null);
+
+        if(cregistros.moveToFirst())
+            do {
+                usuario us = new usuario();
+                us.setId(cregistros.getInt(0));
+                us.setNombres(cregistros.getString(1));
+                us.setApellidos(cregistros.getString(2));
+                us.setFechaNacimiento(cregistros.getString(3));
+                us.setCorreo(cregistros.getString(4));
+                us.setClave(cregistros.getString(5));
+
+                registros.add(us);
+            }while (cregistros.moveToNext());
+
+        return registros;
+    }
+
+
 
 }
--- app/src/main/java/com/example/turisapp/modelos/usuario.java
+++ app/src/main/java/com/example/turisapp/modelos/usuario.java
@@ -1,4 +1,33 @@
 package com.example.turisapp.modelos;
 
+import java.util.Date;
+
 public class usuario {
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String fechaNacimiento;
+    private String correo;
+    private String clave;
+
+    public usuario(){
+    }
+
+    public int getId() {return id;}
+    public void setId (int id) {this.id = id;}
+
+    public String getNombres() {return nombres;}
+    public void setNombres (String nombres) {this.nombres = nombres;}
+
+    public String getApellidos() {return apellidos;}
+    public void setApellidos (String apellidos) {this.apellidos = apellidos;}
+
+    public String getFechaNacimiento() {return fechaNacimiento;}
+    public void setFechaNacimiento (String fechaNacimiento) {this.fechaNacimiento = fechaNacimiento;}
+
+    public String getCorreo() {return correo;}
+    public void setCorreo (String correo) {this.correo = correo;}
+
+    public String getClave() {return clave;}
+    public void setClave (String clave) {this.clave = clave;}
 }
