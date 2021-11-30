954b513b7a1597d9cf083268ab2eb5ec6314616c
Eliana Marín Rincón <90230434+elianamarinrincon@users.noreply.github.com>
2021-11-29 19:01:42 -0500

    Actualización firebase 29/11

--- app/src/main/java/com/example/turisapp/LoginActivity.java
+++ app/src/main/java/com/example/turisapp/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.turisapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth autenticacion;
+    private EditText txtEmail;
+    private EditText txtclave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        autenticacion=FirebaseAuth.getInstance();
 
         Button btnIngresar = (Button) findViewById(R.id.login_btnIngreso);
         EditText txtEmail = (EditText) findViewById(R.id.login_txtEmail);
@@ -47,8 +59,39 @@ public class LoginActivity extends AppCompatActivity {
 
             //@Override
             public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), RegistroActivity.class);
-                startActivity(i);
+                if (validarcamposvacios())
+                {
+                AlertDialog.Builder msj=new AlertDialog.Builder(view.getContext());
+                msj.setMessage("Existen campos vacios");
+                msj.create();
+                msj.show();
+                }
+                else {
+                    autenticacion.createUserWithEmailAndPassword(
+                            txtEmail.getText().toString(),
+                            txtClave.getText().toString()).addOnCompleteListener((Activity) view.getContext(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                           if (task.isSuccessful())
+                           {
+                               Intent menu=new Intent(view.getContext(), PerfilActivity.class);
+                               menu.putExtra("usuario",txtEmail.getText().toString());
+                               startActivity(menu);
+
+                           }
+                           else {
+                               AlertDialog.Builder msj=new AlertDialog.Builder(view.getContext());
+                               msj.setMessage("No se puede registrar el usuario");
+                               msj.create();
+                               msj.show();
+
+                           }
+                        }
+                    });
+                }
+
+                Intent in = new Intent(view.getContext(), RegistroActivity.class);
+                startActivity(in);
             }
         });
 
@@ -62,4 +105,18 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    private boolean validarcamposvacios()
+    {
+        boolean vacio=true;
+        try {
+            if (txtEmail.getText().toString().isEmpty()&& txtclave.getText().toString().isEmpty());
+            vacio=false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        return vacio;
+    }
+
 }
--- app/src/main/java/com/example/turisapp/RegistroActivity.java
+++ app/src/main/java/com/example/turisapp/RegistroActivity.java
@@ -10,18 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.turisapp.clases.Mensajes;
-import com.example.turisapp.clases.UsuarioADO;
-import com.example.turisapp.modelos.usuario;
 
 public class RegistroActivity extends AppCompatActivity {
-
-    private usuario registro=null;
-    private EditText txtNombres;
-    private EditText txtApellidos;
-    private EditText txtFechaNacimiento;
-    private EditText txtEmail;
-    private EditText txtClave;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,54 +21,45 @@ public class RegistroActivity extends AppCompatActivity {
         Button btnVolver = (Button) findViewById(R.id.registro_btnVolver);
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
+            public void onClick(View view) {onBackPressed();}
         });
 
         Button btnGuardar = (Button) findViewById(R.id.registor_btnGuardar);
-        txtNombres = (EditText) findViewById(R.id.registro_txtNombres);
-        txtApellidos = (EditText) findViewById(R.id.registro_txtApellidos);
-        txtFechaNacimiento = (EditText) findViewById(R.id.registro_dateFechaNacimiento);
-        txtEmail = (EditText) findViewById(R.id.registo_emailCorreo);
-        txtClave = (EditText) findViewById(R.id.registro_pswClave);
-
-        Bundle parametros = getIntent().getExtras();
-        if (parametros != null)
-        if (parametros.containsKey("id")) {
-           UsuarioADO db = new UsuarioADO(this);
-           usuario us = db.obtenerUsuario(parametros.getLong("id"));
-           this.registro = us;
-           cargarDatos();
-        }
+        EditText txtNombres = (EditText) findViewById(R.id.registro_txtNombres);
+        EditText txtApellidos = (EditText) findViewById(R.id.registro_txtApellidos);
+        EditText txtFechaNacimiento = (EditText) findViewById(R.id.registro_dateFechaNacimiento);
+        EditText txtEmail = (EditText) findViewById(R.id.registo_emailCorreo);
+        EditText txtClave = (EditText) findViewById(R.id.registro_pswClave);
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            String nombres = txtNombres.getText().toString();
-            String apellidos = txtApellidos.getText().toString();
-            String fechaNacimiento = txtFechaNacimiento.getText().toString();
-            String email = txtEmail.getText().toString();
-            String clave = txtClave.getText().toString();
+                String nombres = txtNombres.getText().toString();
+                String apellidos = txtApellidos.getText().toString();
+                String fechaNacimiento = txtFechaNacimiento.getText().toString();
+                String email = txtEmail.getText().toString();
+                String clave = txtClave.getText().toString();
 
-            if (nombres.isEmpty() || apellidos.isEmpty() || fechaNacimiento.isEmpty() || email.isEmpty() || clave.isEmpty()) {
-               AlertDialog.Builder mensaje = new AlertDialog.Builder(view.getContext());
-               mensaje.setTitle("No se puede guardad");
-               mensaje.setMessage("Por favor diligencia todos los campos");
-               mensaje.create();
-               mensaje.show();
-            }
-            else if (clave.length() < 6) {
-               AlertDialog.Builder mensaje = new AlertDialog.Builder(view.getContext());
-               mensaje.setTitle("Error");
-               mensaje.setMessage("Por favor ingresa una contraseña de 6 o más caracteres");
-               mensaje.create();
-               mensaje.show();
-            }
-            else {
-               Intent i = new Intent(view.getContext(), PerfilActivity.class);
-               startActivity(i);
-            }
+                if (nombres.isEmpty() || apellidos.isEmpty() || fechaNacimiento.isEmpty() || email.isEmpty() || clave.isEmpty()){
+                    AlertDialog.Builder mensaje = new AlertDialog.Builder(view.getContext());
+                    mensaje.setTitle("No se puede guardad");
+                    mensaje.setMessage("Por favor diligencia todos los campos");
+                    mensaje.create();
+                    mensaje.show();
+                }
+
+                else if (clave.length()<6){
+                    AlertDialog.Builder mensaje = new AlertDialog.Builder(view.getContext());
+                    mensaje.setTitle("Error");
+                    mensaje.setMessage("Por favor ingresa una contraseña de 6 o más caracteres");
+                    mensaje.create();
+                    mensaje.show();
+                }
+
+                else{
+                    Intent i = new Intent(view.getContext(), PerfilActivity.class);
+                    startActivity(i);
+                }
 
             }
         });
@@ -87,21 +68,11 @@ public class RegistroActivity extends AppCompatActivity {
     public static boolean validacionClave (String clave)
     {
         boolean validacion = false;
+
         if ((clave.length()>=6))
             validacion = true;
 
         return validacion;
-    }
-
-
-
-    private void cargarDatos()
-    {
-        this.txtNombres.setText(this.registro.getNombres());
-        this.txtApellidos.setText(this.registro.getApellidos());
-        this.txtFechaNacimiento.setText(this.registro.getFechaNacimiento());
-        this.txtEmail.setText(this.registro.getCorreo());
-        this.txtClave.setText(this.registro.getClave());
     }
 
 
