839ad463ded7f4d37acb81c2a60139e5f5a15378
DianaVBA <90658989+DianaVBA@users.noreply.github.com>
2021-11-30 13:08:46 -0500

    Falta mejorar las ventanas y la prueba de usuario

--- app/src/main/java/com/example/turisapp/PerfilActivity.java
+++ app/src/main/java/com/example/turisapp/PerfilActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.turisapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
--- app/src/main/java/com/example/turisapp/RegistroActivity.java
+++ app/src/main/java/com/example/turisapp/RegistroActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.turisapp;
 
 import androidx.appcompat.app.AlertDialog;
