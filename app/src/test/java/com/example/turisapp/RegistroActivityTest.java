bd1a3cacd6e617709ccf182f8aa779204dae2baf
DianaVBA <90658989+DianaVBA@users.noreply.github.com>
2021-11-29 22:21:29 -0500

    Actualización firebase 29/11

---
+++ app/src/test/java/com/example/turisapp/RegistroActivityTest.java
@@ -0,0 +1,11 @@
+//package com.example.turisapp;
+
+import android.media.VolumeShaper;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+
+
+//public class RegistroActivityTest extends TestCase {
+//private VolumeShaper.Operation operation = new VolumeShaper.Operation();
